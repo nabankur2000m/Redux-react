6a295260a65ff2bb8b942d97cd726c3013e35147
NABANKUR MONDAL <76182696+nabankur2000m@users.noreply.github.com>
2024-04-19 22:50:06 +0530

    first commit

---
+++ redux-react.js
@@ -0,0 +1,30 @@
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { counter: state.counter + 1 };
+        case 'decrement':
+            return { counter: state.counter - 1 };
+        case 'INCREMENTBY2':
+            return { counter: state.counter + 2 };
+        case 'DECREMENTBY2':
+            return { counter: state.counter - 2 };
+        default:
+            return state;
+    }
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+    const latestState = store.getState();
+    console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
+store.dispatch({ type: 'INCREMENTBY2' });
+store.dispatch({ type: 'DECREMENTBY2' });
